b571e974b4ce6f88d021f7578d6123662db98ac8
thomasms <stainer.tom@gmail.com>
2021-04-08 16:16:51 +0200

    Work in progress

--- src/App.js
+++ src/App.js
@@ -1,35 +1,29 @@
 import React from "react";
-
-import Grid from "@material-ui/core/Grid";
-
-import AppNavBar from "./AppNavBar";
-import AppCard from "./AppCard";
+import { Label, NumericInput } from "@blueprintjs/core";
 
 import "./App.css";
+
+import "@blueprintjs/core/lib/css/blueprint.css";
 
 function App() {
   return (
     <div className="app">
-      <AppNavBar position="sticky" />
-      <div className="app-main">
-        <Grid
-          container
-          spacing={2}
-          direction="row"
-          justify="space-evenly"
-          alignItems="center"
-          style={{}}
-        >
-          <Grid item xs={12} md={6} lg={4}>
-            <AppCard elevation={3} width={64} height={64} />{" "}
-          </Grid>
-          <Grid item xs={12} md={6} lg={4}>
-            <AppCard elevation={3} width={64} height={64} />{" "}
-          </Grid>
-          <Grid item xs={12} md={6} lg={4}>
-            <AppCard elevation={3} width={64} height={64} />{" "}
-          </Grid>
-        </Grid>
+      <div className="input-salary">
+        <Label style={{ fontSize: 20 }}>
+          Gross annual salary
+          <NumericInput
+            min={0}
+            fill={false}
+            disabled={false}
+            large={true}
+            leftIcon={"circle"}
+            buttonPosition={"none"}
+            placeholder={"£ " + (30000).toLocaleString()}
+            locale={"en-US"}
+            majorStepSize={1000}
+            stepSize={1000}
+          />
+        </Label>
       </div>
     </div>
   );
